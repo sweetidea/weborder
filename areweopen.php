0702173770bfc401ed30cb1ccc9ef7a1680d7598
John Piermarini <ohnoabigshark@gmail.com>
2013-02-25 17:51:28 -0500

    Fixed time offset
    The server time offset got messed up and is now fixed. Open/close should
    work properly on the web order widget.

--- areweopen.php
+++ areweopen.php
@@ -7,10 +7,10 @@ include_once('include/init.php');
 //day 0 is Sunday
 
 $response->currentlyOpen = false;
+$serverTimeOffset = 2; //server is 2 hours behind EST
 $time = date("w G i"); //day, hour, minute
 $time = explode(" ",$time);
-$time[1] = ($time[1]+2)%24; //correction for the current hour. Server is 2 hours behind
-if($time[1] >= 0 && $time[1] <= 3) { //correction for the day of the week if time ahs already brought us to the next day
+if( $time[1] <= 1) { //correction for the day of the week if time ahs already brought us to the next day
 	$time[0] = ($time[0]-1)%7;
 }
 /*$response->day = date("w");
@@ -24,13 +24,13 @@ $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 if(count($result)!=1) { //if we can't find hours for the day, we're definitely closed and should abort. 
 	echo json_encode($response);
 	return;
-}
 
+}
 $result = $result[0];
 $response->day = $result['day'];
 $response->open = $result['open'];
 $response->close = $result['close'];
-$timeToCompare = ($time[1]*100)+$time[2];
+$timeToCompare = ((($time[1]+$serverTimeOffset)%24)*100)+$time[2];
 $response->timeToCompare = $timeToCompare;
 if($timeToCompare >= $response->open || $timeToCompare <= $response->close) {
 	$response->currentlyOpen = true;
